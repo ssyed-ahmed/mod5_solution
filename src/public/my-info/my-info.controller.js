3c1690a45e1e401eaae96930429e27805c9c46cf
Sumair <sumair@coursera.com>
2016-11-24 22:17:07 +1100

    mod files

--- src/public/my-info/my-info.controller.js
+++ src/public/my-info/my-info.controller.js
@@ -10,11 +10,25 @@
 
     $ctrl.myInfo = {};
 
-    var myInfoObj = MyInfoService.getMyInfo();
-    $ctrl.myInfo.firstName = myInfoObj.firstName;
-    $ctrl.myInfo.lastName = myInfoObj.lastName;
-    $ctrl.myInfo.email = myInfoObj.email;
-    $ctrl.myInfo.phone = myInfoObj.phone;
-    $ctrl.myInfo.favouriteMenu = myInfoObj.favouriteMenu;
+    $ctrl.isUserRegistered = function() {
+      var myInfoObj = MyInfoService.getMyInfo();
+      if ($ctrl.isObjectEmpty(myInfoObj)) {
+        return false;
+      }
+      $ctrl.isRegistered = true;
+      $ctrl.myInfo.firstName = myInfoObj.firstName;
+      $ctrl.myInfo.lastName = myInfoObj.lastName;
+      $ctrl.myInfo.email = myInfoObj.email;
+      $ctrl.myInfo.phone = myInfoObj.phone;
+      $ctrl.myInfo.favouriteMenu = myInfoObj.favouriteMenu;
+      return true;
+    };
+
+    $ctrl.isObjectEmpty = function(obj) {
+      if(Object.getOwnPropertyNames(obj).length > 0) {
+        return false;
+      }
+      return true;
+    };
   }
 })();
